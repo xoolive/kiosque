d415731b1fcf92cedf124e9e55634343ce01e35e
Xavier Olive <git@xoolive.org>
2021-08-09 01:20:16 +0200

    new paper

---
+++ kiosque/website/mediapart.py
@@ -0,0 +1,45 @@
+from ..core.website import Website
+
+
+class Mediapart(Website):
+    base_url = "https://www.mediapart.fr/"
+    login_url = "https://www.mediapart.fr/login_check"
+
+    author_meta = {"name": ["author"]}
+
+    @property
+    def login_dict(self):
+        credentials = self.credentials
+        assert credentials is not None
+
+        return dict(
+            name=credentials["username"],
+            password=credentials["password"],
+            op="Se+connecter",
+        )
+
+    def article(self, url):
+        e = self.bs4(url)
+        article = e.find("div", {"class": "content-article"})
+
+        # if not logged in
+        embedded = article.find("div", {"class": "content-article"})
+        if embedded is not None:
+            article = embedded
+
+        # if logged in
+        embedded = article.find("div", {"class": "page-pane"})
+        if embedded is not None:
+            article = embedded
+
+        return article
+
+    def clean(self, article):
+        article = super().clean(article)
+        article.name = "article"
+        article.attrs.clear()
+
+        for elem in article.find_all("div"):
+            elem.decompose()
+
+        return article
