a247332a511dcd5c7eb6069036f4d5786d373aaf
Xavier Olive <git@xoolive.org>
2021-08-11 02:13:22 +0200

    new paper

---
+++ kiosque/website/asahi.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+from ..core.website import Website
+
+
+class Asahi(Website):
+    base_url = "https://www.asahi.com"
+
+    header_entries = ["title", "date", "url"]
+
+    article_node = ("div", {"class": "_3YqJ1"})
+    clean_attributes = ["a"]
+
+    def clean(self, article):
+        article = super().clean(article)
+
+        new_article = BeautifulSoup("<article></article>", features="lxml")
+
+        for elem in article.find_all("p"):
+            new_article.append(elem)
+
+        return new_article
--- kiosque/website/nikkei.py
+++ kiosque/website/nikkei.py
@@ -2,6 +2,15 @@ from ..core.website import Website
 
 
 class Nikkei(Website):
+    base_url = "https://www.nikkei.com"
+
+    header_entries = ["title", "date", "url"]
+
+    def article(self, url):
+        return self.bs4(url).find("article").find("section")
+
+
+class NikkeiAsia(Website):
     base_url = "https://asia.nikkei.com/"
 
     article_node = ("div", {"class": "ezrichtext-field"})
